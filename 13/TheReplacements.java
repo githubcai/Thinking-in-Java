1dec4e182e0bb6ce256ae956d59035ae5159302d
caixukun <2468085704@qq.com>
2015-10-20 23:55:22 +0800

    d

---
+++ 13/TheReplacements.java
@@ -0,0 +1,26 @@
+import java.util.regex.*;
+
+/*!                 Here's a       block of text to use as input         to the regular expression matcher. Note that we'll first extract the block of text by looking for the special delimiters, then process the extracted block.                         !*/
+
+public class TheReplacements{
+    public static void main(String[] args) throws Exception{
+        String s = TextFile.read("TheReplacements.java");
+        Matcher mInput = Pattern.compile("/\\*!(.*)!\\*/", Pattern.DOTALL).matcher(s);
+        if(mInput.find()){
+            s = mInput.group(1);
+        }
+        
+        s = s.replaceAll(" {2,}", " ");
+        s = s.replaceAll("(?m)^ +", "");
+        System.out.println(s);
+        s = s.replaceFirst("[aeiou]", "(VOWEL1)");
+        StringBuffer sbuf = new StringBuffer();
+        Pattern p = Pattern.compile("[aeiou]");
+        Matcher m = p.matcher(s);
+        while(m.find()){
+            m.appendReplacement(sbuf, m.group().toUpperCase());
+        }
+        m.appendTail(sbuf);
+        System.out.println(sbuf);
+    }
+}
