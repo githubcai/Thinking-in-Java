856f0fad262d0d2ead53072ded419543789444bf
caixukun <2468085704@qq.com>
2015-11-11 21:11:12 +0800

    a

--- 21/ThreadVariations.java
+++ 21/ThreadVariations.java
@@ -25,7 +25,7 @@ class InnerThread1{
             return getName() + ": " + countDown;
         }
     }
-    pubic InnerThread1(String name){
+    public InnerThread1(String name){
         inner = new Inner(name);
     }
 }
@@ -63,6 +63,94 @@ class InnerRunnable1{
         Thread t;
         Inner(String name){
             t = new Thread(this, name);
+            t.start();
         }
+        public void run(){
+            try{
+                while(true){
+                    System.out.println(this);
+                    if(--countDown==0){
+                        return;
+                    }
+                    TimeUnit.MILLISECONDS.sleep(10);
+                }
+            }catch(InterruptedException e){
+                System.out.println("sleep() interrupted");
+            }
+        }
+        public String toString(){
+            return t.getName() + ": " + countDown;
+        }
+    }
+    public InnerRunnable1(String name){
+        inner = new Inner(name);
+    }
+}
+
+class InnerRunnable2{
+    private int countDown = 5;
+    private Thread t;
+    public InnerRunnable2(String name){
+        t = new Thread(new Runnable(){
+            public void run(){
+                try{
+                    while(true){
+                        System.out.println(this);
+                        if(--countDown==0){
+                            return;
+                        }
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    }
+                }catch(InterruptedException e){
+                    System.out.println("sleep() interrupted");
+                }
+            }
+            public String toString(){
+                return Thread.currentThread().getName() + ": " + countDown;
+            }
+        }, name);
+        t.start();
+    }
+}
+
+class ThreadMethod{
+    private int countDown = 5;
+    private Thread t;
+    private String name;
+    public ThreadMethod(String name){
+        this.name = name;
+    }
+    public void runTask(){
+        if(t==null){
+            t = new Thread(name){
+                public void run(){
+                    try{
+                        while(true){
+                            System.out.println(this);
+                            if(--countDown==0){
+                                return;
+                            }
+                            sleep(10);
+                        }
+                    }catch(InterruptedException e){
+                        System.out.println("sleep() interrupted");
+                    }
+                }
+                public String toString(){
+                    return getName() + ": " + countDown;
+                }
+            };
+            t.start();
+        }
+    }
+}
+
+public class ThreadVariations{
+    public static void main(String[] args){
+        new InnerThread1("InnerThread1");
+        new InnerThread2("InnerThread2");
+        new InnerRunnable1("InnerRunnable1");
+        new InnerRunnable2("InnerRunnable2");
+        new ThreadMethod("ThreadMethod").runTask();
     }
 }
